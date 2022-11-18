0363ce4031060d63cbc7efea01b5f7588e26662d
DiegoAlonso92 <diego.alonso.uy@gmail.com>
2022-11-18 20:25:30 -0300

    constructor de socio

--- mainjs/app.js
+++ mainjs/app.js
@@ -3,26 +3,7 @@ function saludar() {
 }
 saludar();
 
-
-
-//Ver como queda mejor, this.numFuncionario = parseFloat (numFuncionario);
-class socioNum { 
-    constructor (nombreIngresado, apellidoIngresado, sucursalIngresda, cargoIngresado, afiliado) {
-        this.nombreIngresado = nombreIngresado,
-        this.apellidoIngresado = apellidoIngresado,
-        this.sucursalIngresda = sucursalIngresda,
-        this.cargoIngresado = cargoIngresado,
-        this.afiliado = afiliado
-    }
-}
-    const listaSocios = [];
-    function ingresarSocio (socioNum) {
-        listaSocios.push(new parseFloat (socioNum ()))
-    }
-    
-// la idea es crear un programa interactivo que le solicite al usuario Nombre; Apellido; Numero de funcionario; Si es o no es socio del sindicado; Sucursal donde trabaja; Cargo ejercido y luego mediante un alert le muestre los datos que ingresó.
-
-
+const listaSocios = [];
 
 
 let numFuncionario = prompt("Ingresa tu número de funcionario:");
@@ -37,7 +18,31 @@ if (afiliado != "si") {
 
 alert("Estos fueron los datos ingresados:                                                     " + "Nombre: " + nombreIngresado + "      " + "Apellido: " + apellidoIngresado + "      " + "Sucursal: " + sucursalIngresda + "      " + "Cargo: " + cargoIngresado + "      " + "Numero de funcionario: " + numFuncionario);
 
+
+//Ver como queda mejor, this.numFuncionario = parseFloat (numFuncionario);
+class SocioNum { 
+    constructor (nombreIngresado, apellidoIngresado, sucursalIngresda, cargoIngresado, afiliado) {
+        this.nombreIngresado = nombreIngresado,
+        this.apellidoIngresado = apellidoIngresado,
+        this.sucursalIngresda = sucursalIngresda,
+        this.cargoIngresado = cargoIngresado,
+        this.afiliado = afiliado
+    }
+}
+
+const socio = new SocioNum (nombreIngresado, apellidoIngresado, sucursalIngresda, cargoIngresado, afiliado);
+
+
+function ingresarSocio () {
+    listaSocios.push(socio)
+}
+ingresarSocio ();
+
+
+console.log(listaSocios);
+
+
+
+
+// la idea es crear un programa interactivo que le solicite al usuario Nombre; Apellido; Numero de funcionario; Si es o no es socio del sindicado; Sucursal donde trabaja; Cargo ejercido y luego mediante un alert le muestre los datos que ingresó.
 // como segundo avance del proyecto pretendo crear un Array que almacene la información ingresada por los usuarios.
-
-
-
