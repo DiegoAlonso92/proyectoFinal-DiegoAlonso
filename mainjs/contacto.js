493a9fe24d9588acff597d7e8dddd4fa327384dc
DiegoAlonso92 <diego.alonso.uy@gmail.com>
2022-12-28 11:35:43 -0300

    avance

--- mainjs/contacto.js
+++ mainjs/contacto.js
@@ -12,26 +12,37 @@ const btnEnviar = document.querySelector('#btnEnviar');
 // (setitem es para guardar, getitem es para obtener la información que está guardada en LS).
 // JSON permite convertir los objetos a texto plano o string.
 //(JSON.stringfy() permite convertir un objeto o array a string )
+
+
+
 let listaSocios = [];
-let listaSociosLS = JSON.parse(localStorage.getItem('listaSocios'));
-if (listaSocios === 0 || listaSociosLS !== null) {
-    listaSocios = listaSociosLS
+const listaSociosLS = JSON.parse(localStorage.getItem('listaSocios'));
+function addListaSocios() {
+    if (listaSocios === 0 || listaSocios === null || listaSocios === '') {
+        listaSocios = listaSociosLS
+    }
+    else if (listaSociosLS === 0 || listaSociosLS === null || listaSociosLS === '') {
+        fetch("/mainjs/socios-activos.json")
+            .then(response => response.json())
+            .then(data => {
+                localStorage.setItem('listaSocios', JSON.stringify(data))
+                JSON.parse(localStorage.getItem('listaSocios'))
+            })
+    }
 }
-listaSociosLS
-if (listaSocios === 0 || listaSociosLS !== null){
-    fetch("/mainjs/socios-activos.json")
-    .then(response => response.json())
-    .then(data => {        
-        localStorage.setItem ('listaSocios', JSON.stringify(data))
-        const dataEnLs = JSON.parse(localStorage.getItem('listaSocios'))
-    })
-}
+addListaSocios();
 
 console.log(listaSocios);
 
+
+
+
+
+
+
 myForm.addEventListener('submit', (event) => {
     event.preventDefault()
- 
+
     if (inputNumFunc.value === '' || inputNombre.value === '' || inputApellido.value === '' || inputSucursal.value === '' || inputCargo.value === '' || inputMail.value === '' || inputTel.value === '') {
         return
     }
@@ -42,7 +53,7 @@ myForm.addEventListener('submit', (event) => {
             text: 'Ese número de socio ya está registrado.',
             icon: 'error',
             confirmButtonText: 'Entendido'
-          })
+        })
         return
     }
 
@@ -53,7 +64,7 @@ myForm.addEventListener('submit', (event) => {
             text: 'Ese Email ya se encuentra registrado.',
             icon: 'error',
             confirmButtonText: 'Entendido'
-          })
+        })
         return
     }
 
@@ -72,40 +83,3 @@ myForm.addEventListener('submit', (event) => {
     myForm.reset()
     console.log(listaSocios)
 })
-
-// const myFormCanasta = document.querySelector('#my-form-canasta');
-// const inputNumFuncCan = document.querySelector('#NumFuncCan');
-// const inputNombreBebe = document.querySelector('#NombreBebe');
-// const inputApellidosBebe = document.querySelector('#ApellidosBebe');
-// const btnEnviarCan = document.querySelector('#btnEnviarCan');
-
-// let listaCanasta = [];
-// let listaCanastaLS = JSON.parse(localStorage.getItem('listaCanasta'));
-// if (listaCanasta.length === 0) {
-//     listaCanasta = listaCanastaLS
-// }
-// console.log(myFormCanasta);
-    
-
-// myFormCanasta.addEventListener('submit', (event) => {
-//     event.preventDefault()
-//     if (inputNumFuncCan.value === '' || inputNombreBebe.value === '' || inputApellidosBebe.value === '') {
-//         return
-//     }
-//     const socioRepetido = listaCanasta.some((user) => user.numero === inputNumFuncCan.value);
-    
-//     if (socioRepetido) {
-//         alert('Ese número de socio ya ha solicitado la canasta.')
-//         return
-//     }
-//     const socio = {
-//         numero: inputNumFuncCan.value,
-//         nombre_de_recien_nacido: inputNombreBebe.value,
-//         apellidos_de_recien_nacido: inputApellidosBebe.value,
-//     }
-//     listaCanasta.push(socio)
-//     localStorage.setItem('listaCanasta', JSON.stringify(listaCanasta))
-//     myForm.reset()
-//     console.log(listaCanasta)
-// })
-// Agregar un fetch que permita corroborar si el numero de socio se encuentra en la lista de socios activos.
